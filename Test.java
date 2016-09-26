421abcf80e2ae375b4f426f6934d1a8c6864ad21
Arpita30 <arpita.salecha.jain@gmail.com>
2016-09-26 15:15:38 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	
+		 int x; int y;
+		 String name;
+		 public static void main(String args[]) {
+		 Test pnt = new Test();
+		 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+		} 
+
+
+}
+// it prints option d 'null 0 0' 
